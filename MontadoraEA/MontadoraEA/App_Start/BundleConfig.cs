a269c79dbfbfbe5f6f62e3594d52a73cb82cf2bd
Halife <halife.alencar@gmail.com>
2018-07-11 13:15:49 -0400

    datatable funciona, corrigir layout

--- MontadoraEA/MontadoraEA/App_Start/BundleConfig.cs
+++ MontadoraEA/MontadoraEA/App_Start/BundleConfig.cs
@@ -20,11 +20,20 @@ namespace MontadoraEA
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery.dataTables").Include(
+                        "~/Scripts/DataTables/jquery.dataTables.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap4.min.css"));
+
+            
+
+            
+
         }
     }
 }
